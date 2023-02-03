0d6668dc87b84f56faf2769503b0ae161beaa7e5
Marcos <madasilva195@gmail.com>
2023-02-03 10:22:17 -0500

    ViewCourses Activity created

--- app/src/main/java/com/example/sqliteadd/MainActivity.java
+++ app/src/main/java/com/example/sqliteadd/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.sqliteadd;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     // creating variables for our edittext, button and dbhandler
     private EditText courseNameEdt, courseTracksEdt, courseDurationEdt, courseDescriptionEdt;
